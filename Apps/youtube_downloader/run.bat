31edd2d5bf2ace42985d47f33cd5b77a6609c37a
anigno <anigno@gmail.com>
2023-03-07 17:45:14 +0200

    fix running python from command line

--- Apps/youtube_downloader/run.bat
+++ Apps/youtube_downloader/run.bat
@@ -1,3 +1,6 @@
 set /p url=youtube URL:
 set /p output=output folder:
-python youtube_downloader.py -u "%url%" -o "%output%"
+' python youtube_downloader.py -u "%url%" -o "%output%"
+set pythonpath=D:\DEV\GIT\github-python\
+python.exe D:\DEV\GIT\github-python\Apps\youtube_downloader\youtube_downloader.py  -u "%url%"  -o "%output%"
+pause
