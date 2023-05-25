02f7d338e0c587ce339915e9e0dc052e5458ed59
anigno <anigno@gmail.com>
2023-05-25 23:39:20 +0300

    removed old apps

--- Apps/AnignoraHomeAssistant/run anignora server.bat
+++
@@ -1,5 +0,0 @@
-d:
-cd D:\cloud\mega\Dev\python\AnignoraHomeAssistant
-python flask_server.py
-
-pause
--- Apps/test_apps/test_app_1.py
+++ Apps/test_apps/test_app_1.py
@@ -1,1 +1,7 @@
-print("hello")
+import hashlib
+
+string = "aronanigno136"
+hash_object = hashlib.sha256(string.encode())
+hex_dig = hash_object.hexdigest()
+print(hex_dig)
+
