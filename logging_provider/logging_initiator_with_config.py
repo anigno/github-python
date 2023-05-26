6e0f0af94d478e897465932b493e9875f3bbff5f
anigno <anigno@gmail.com>
2023-05-26 20:35:40 +0300

    logging provider added datagram logging and service

---
+++ logging_provider/logging_initiator_with_config.py
@@ -0,0 +1,19 @@
+import logging.config
+import json
+from logging_provider.logging_initiator_base import LoggingInitiatorBase
+import random
+import time
+class LoggingInitiatorWithConfig(LoggingInitiatorBase):
+
+    def __init__(self, logging_config_file: str):
+        super().__init__()
+        logging.config.dictConfig(json.load(open(logging_config_file, 'r')))
+
+if __name__ == '__main__':
+    LoggingInitiatorWithConfig('logging_config.json')
+    logger = logging.getLogger(LoggingInitiatorBase.DATAGRAM_LOGGER)
+    for i in range(400):
+        level = random.randint(0, 5) * 10
+        long_string = '123456789_' * 300
+        logger.log(level=level, msg=f'{i} {long_string}')
+        time.sleep(1)
