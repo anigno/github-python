834428b0067ba8e14db67c08a5b468acc38721e8
Aharon Gina <anigno@gmail.com>
2023-06-28 15:10:25 +0300

    clickhouse integration examples

--- PythonExamples/ClickHouseAndMongoEngine/clickhouse_integration.py
+++ PythonExamples/ClickHouseAndMongoEngine/clickhouse_integration.py
@@ -1,8 +1,9 @@
 import random
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 from clickhouse_driver import Client
 from PythonExamples.ClickHouseAndMongoEngine.helpers import measure_time
+import csv
 
 def execute(query, data=None, is_show_list_results=True):
     print(f'****** {query}')
@@ -18,68 +19,151 @@ def execute(query, data=None, is_show_list_results=True):
     print(f'------ {time.time() - start} sec')
     print()
 
-@measure_time
-def insert_rows(table, count):
+# @measure_time
+def generate_and_insert_rows(table, n_cells, n_counters, start_time, end_time):
+    print(f'generate_and_insert_rows {start_time} - {end_time}')
     rows = []
-    for a in range(count):
+    for a in range(n_cells):
+        market = f'market{str(a // 6000)}'
         row = [f'cell{a}',
-               datetime(random.randint(2000, 2024), 1, 2, 3, 4, 5),
-               datetime(random.randint(2000, 2024), 1, 2, 3, 4, 5),
+               # datetime(random.randint(2000, 2024), 1, 2, 3, 4, 5),
+               # datetime(random.randint(2000, 2024), 1, 2, 3, 4, 5),
+               start_time,
+               end_time,
                {'counter1': random.randint(0, 100), 'counter2': 20.7},
-               'vendor1', 'market1']
+               f'vendor{random.randint(0, 4)}', market]
+        counters: dict = row[3]
+        # add counters
+        for b in range(n_counters):
+            counters[f'counter{b}'] = random.randint(0, 1000) / 10
         rows.append(row)
     insert_query = f'INSERT INTO {table} (cell_id, start_time, end_time, counter_data, vendor, market) VALUES '
     execute(insert_query, rows)
 
+def generate_day_data(table, n_cells, n_counters, n_days):
+    start_time = datetime(2023, 1, 1, 0, 0, 0)
+    end_time = datetime(2023, 1, 1, 0, 15, 0)
+    for a in range(n_days):
+        for b in range(24):
+            for c in range(4):
+                generate_and_insert_rows(table, n_cells, n_counters, start_time, end_time)
+                start_time += timedelta(minutes=15)
+                end_time += timedelta(minutes=15)
+
+def get_csv_header(filepath):
+    with open(filepath, 'r') as file:
+        reader = csv.reader(file)
+        header_row = next(reader)
+        return header_row
+
+def generate_rows_from_csv(filepath):
+    with open(filepath, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)
+        for row in reader:
+            yield row
+
+def generate_chunks_from_rows(rows, chunk_size):
+    counter = 0
+    chunk_rows = []
+    for row in rows:
+        chunk_rows.append(row)
+        counter += 1
+        if counter % chunk_size == 0:
+            yield chunk_rows
+            chunk_rows = []
+    yield chunk_rows
+
+def create_data_row_from_chunk_row(chunk_row, header_row) -> list:
+    datetime_conversion_string = '%Y-%m-%d %H:%M:%S'
+    start_time = datetime.strptime(chunk_row[1], datetime_conversion_string)
+    end_time = datetime.strptime(chunk_row[2], datetime_conversion_string)
+    row = [chunk_row[0], start_time, end_time]
+    counters_dict = {}
+    for i, counter_value in enumerate(chunk_row[3:-1]):
+        counters_dict[header_row[i + 3]] = float(counter_value) if counter_value != '\\N' else 0.0
+    row.append(counters_dict)
+    row.append(chunk_row[45])
+    row.append('market_1')
+    return row
+
+def get_data_rows(chunk, header_row):
+    data_rows = []
+    for chunk_row in chunk:
+        data_row = create_data_row_from_chunk_row(chunk_row, header_row)
+        data_rows.append(data_row)
+    return data_rows
+
+def insert_chunk_rows_to_db(table, data_rows):
+    print(f'inserting data rows with size: {len(data_rows)}')
+    insert_query = f'INSERT INTO {table} (cell_id, start_time, end_time, counter_data, vendor, market) VALUES '
+    execute(insert_query, data_rows)
+
+def insert_from_csv(table_name):
+    csv_file = '/home/aharongina/Downloads/pm_export.csv'  # 8295708 rows
+    header_row = get_csv_header(csv_file)
+    rows_generator = generate_rows_from_csv(csv_file)
+    chunks_generator = generate_chunks_from_rows(rows_generator, 500000)
+    for chunk in chunks_generator:
+        data_rows = get_data_rows(chunk, header_row)
+        insert_chunk_rows_to_db(table_name, data_rows)
+
 if __name__ == '__main__':
     client = Client(host='localhost', port=9000, user='default', password='')
-    database_name = 'sample_database'
-    table_name = 'counter_data_map_schema'
 
-    execute(f'DROP DATABASE IF EXISTS {database_name}')
-    execute(f'CREATE DATABASE IF NOT EXISTS {database_name}')
-    execute(f'USE {database_name}')
-    execute(f'DROP TABLE IF EXISTS {table_name}')
+    execute(f'DROP DATABASE IF EXISTS sample_database')
+    execute(f'CREATE DATABASE IF NOT EXISTS sample_database')
+    execute(f'USE sample_database')
+    execute(f'DROP TABLE IF EXISTS table_15_markets_snapshot_one')
 
     table_create_query = f'''
-                    CREATE TABLE {table_name} (
+                    CREATE TABLE table_15_markets_snapshot_one (
+                        cell_id String,
                         start_time DateTime,
                         end_time DateTime,
-                        cell_id String,
                         counter_data Map(String, Float32),
                         vendor String,
                         market String
                     ) ENGINE = MergeTree()
-                    ORDER BY (start_time,end_time )
-                    PRIMARY KEY (start_time,end_time)
+                    ORDER BY (start_time,cell_id )
+                    PRIMARY KEY (start_time,cell_id)
                     PARTITION BY market;
                 '''
+    # table_create_query = f'''
+    #                 CREATE TABLE {table_name} (
+    #                     cell_id String,
+    #                     start_time DateTime,
+    #                     end_time DateTime,
+    #                     counter_data Map(String, Float32),
+    #                     vendor String,
+    #                     market String
+    #                 ) ENGINE = MergeTree()
+    #                 ORDER BY (start_time,cell_id )
+    #                 PRIMARY KEY (start_time,cell_id)
+    #                 PARTITION BY market;
+    #             '''
     execute(table_create_query)
-
     execute(f'SHOW TABLES')
-    execute(f'DESCRIBE {table_name}')
+    execute(f'DESCRIBE table_15_markets_snapshot_one')
 
-    insert_rows(table_name, 1_000_000)
-    execute(f'SELECT * FROM {table_name}', is_show_list_results=False)
-    execute(f"SELECT * FROM {table_name} WHERE start_time>'2012-06-18'", is_show_list_results=False)
-    execute(f"SELECT * FROM {table_name} WHERE counter_data['counter1'] > 50", is_show_list_results=False)
+    # generate_and_insert_rows('table_15_snapshot', 100000, 100,datetime(2023,1,1,0,0,0),datetime(2023,1,1,0,15,0))
+    generate_day_data('table_15_markets_snapshot_one', 100000, 100, 1)
+    # execute(f'SELECT * FROM {table_name}', is_show_list_results=False)
+    # execute(f"SELECT * FROM {table_name} WHERE start_time>'2012-06-18'", is_show_list_results=False)
+    # execute(f"SELECT * FROM {table_name} WHERE counter_data['counter1'] > 50", is_show_list_results=False)
 
-    # def get_data_size(table_name):
-    #     # query = f'''SELECT database, table, formatReadableSize(sum(data_compressed_bytes)) AS total_size
-    #     #         FROM system.parts GROUP BY database, table'''
-    #     # WHERE active
-    #     #   AND database = 'test_database'
-    #     #   AND table = '{table_name}'
-    #     # GROUP BY database, table'''
-    #     # query='select  * from test_database'
-    #     # results = ch.run_query(query)
-    #
-    #     query = "SELECT formatReadableSize(database_size) AS size FROM test_database.databases WHERE name = 'test_database'"
-    #     query = """
-    #         SELECT sum(data_compressed_bytes) AS size
-    #         FROM system.parts
-    #         WHERE database = 'test_database'
-    #     """
-    #
-    #     result = client.execute(query)
-    #     print(result)
+    table_size_query = f'''
+        SELECT table, formatReadableSize(size) as size, rows, days, formatReadableSize(avgDaySize) as avgDaySize FROM (
+        SELECT
+            table,
+            sum(bytes) AS size,
+            sum(rows) AS rows,
+            min(min_date) AS min_date,
+            max(max_date) AS max_date,
+            (max_date - min_date) AS days,
+            size / (max_date - min_date) AS avgDaySize
+        FROM system.parts
+        WHERE active 
+        GROUP BY table
+        ORDER BY rows DESC)'''
+    execute(table_size_query)
