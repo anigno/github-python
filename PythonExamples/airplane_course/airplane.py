e86fe394b49f6caf8c35e706627028b20b88a61f
aharon gina <44746052+anigno@users.noreply.github.com>
2023-12-18 14:09:18 +0200

    airplane

--- PythonExamples/airplane_course/airplane.py
+++ PythonExamples/airplane_course/airplane.py
@@ -4,28 +4,25 @@ from PythonExamples.airplane_course.Location3d import Location3d
 from PythonExamples.airplane_course.direction3d import Direction3d
 
 class Airplane:
-    def __init__(self, name: str, location: Location3d, direction: Direction3d, max_direction_delta: Direction3d, velocity=0.0):
+    def __init__(self, name: str, location: Location3d, direction: Direction3d, velocity=0.0):
         self.name = name
         self.location = location
         self.direction = direction
-        self.max_direction_delta = max_direction_delta
         self.velocity = velocity
 
-    def move_next(self, interval_sec):
-        self.location.x = self.location.x + math.cos(self.direction.azimuth) * math.cos(self.direction.elevation) * self.velocity * interval_sec
-        self.location.y = self.location.y + math.sin(self.direction.azimuth) * math.cos(self.direction.elevation) * self.velocity * interval_sec
-        self.location.h = self.location.h + math.sin(self.direction.elevation) * self.velocity * interval_sec
+    def move(self, interval_sec):
+        self.location.x += math.cos(self.direction.azimuth) * math.cos(self.direction.elevation) * self.velocity * interval_sec
+        self.location.y += math.sin(self.direction.azimuth) * math.cos(self.direction.elevation) * self.velocity * interval_sec
+        self.location.h += math.sin(self.direction.elevation) * self.velocity * interval_sec
 
-    def move_next_to_point(self, interval_sec, location: Location3d):
-        dx = location.x - self.location.x
-        dy = location.y - self.location.y
-        dh = location.h - self.location.h
-        dx = 0.000001 if dx == 0 else  dx
-        req_az = math.atan(dy / dx)
-        req_el = math.atan(dh / dx)
-        self.direction.azimuth = req_az
-        self.direction.elevation = req_el
-        self.move_next(interval_sec)
+    def calculate_Direction(self, location: Location3d) -> Direction3d:
+        delta_x = location.x - self.location.x
+        delta_y = location.y - self.location.y
+        delta_z = location.h - self.location.h
+        azimuth = math.atan2(delta_y, delta_x)
+        horizontal_distance = math.sqrt(delta_x ** 2 + delta_y ** 2)
+        elevation = math.atan2(delta_z, horizontal_distance)
+        return Direction3d(azimuth, elevation)
 
     def __str__(self):
         str_list = []
@@ -36,79 +33,69 @@ class Airplane:
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
-    def test_move_next():
-        global a
-        a = Airplane('1 plane1', Location3d(0.0, 0.0, 0.0), Direction3d(0.0, 0.0), Direction3d(), velocity=1.0)
+    epsilon = 0.01
+
+    def test_move():
+        a = Airplane('1 plane1', Location3d(0.0, 0.0, 0.0), Direction3d(0.0, 0.0), velocity=1.0)
         print(a)
-        a.move_next(1.0)
+        a.move(1.0)
         print(a)
-        assert abs(a.location.x - 1) < 0.01
-        a = Airplane('2 plane1', Location3d(0.0, 0.0, 0.0), Direction3d(0.0, 0.0), Direction3d(), velocity=1.0)
+        assert abs(a.location.x - 1) < epsilon
+        a = Airplane('2 plane1', Location3d(0.0, 0.0, 0.0), Direction3d(0.0, 0.0), velocity=1.0)
         a.direction.azimuth = math.pi / 2
-        a.move_next(1.0)
+        a.move(1.0)
         print(a)
-        assert abs(a.location.y - 1) < 0.01
-        a = Airplane('3 plane1', Location3d(0.0, 0.0, 0.0), Direction3d(0.0, 0.0), Direction3d(), velocity=1.0)
+        a = Airplane('3 plane1', Location3d(0.0, 0.0, 0.0), Direction3d(0.0, 0.0), velocity=1.0)
         a.direction.elevation = math.pi / 4
-        a.move_next(1.0)
+        a.move(1.0)
         print(a)
-        assert abs(a.location.x - 0.7071) < 0.01
-        assert abs(a.location.h - 0.7071) < 0.01
-        a = Airplane('4 plane1', Location3d(0.0, 0.0, 0.0), Direction3d(0.0, 0.0), Direction3d(), velocity=1.0)
+        assert abs(a.location.x - 0.7071) < epsilon
+        assert abs(a.location.h - 0.7071) < epsilon
+        a = Airplane('4 plane1', Location3d(0.0, 0.0, 0.0), Direction3d(0.0, 0.0), velocity=1.0)
         a.direction.azimuth = math.pi / 4
         a.direction.elevation = math.pi / 4
-        a.move_next(1.0)
+        a.move(1.0)
         print(a)
-        assert abs(a.location.x - 0.5) < 0.01
-        assert abs(a.location.y - 0.5) < 0.01
-        assert abs(a.location.h - 0.7071) < 0.01
-        a = Airplane('5 plane1', Location3d(0.0, 0.0, 0.0), Direction3d(0.0, 0.0), Direction3d(), velocity=1.0)
+        assert abs(a.location.x - 0.5) < epsilon
+        assert abs(a.location.y - 0.5) < epsilon
+        assert abs(a.location.h - 0.7071) < epsilon
+        a = Airplane('5 plane1', Location3d(0.0, 0.0, 0.0), Direction3d(0.0, 0.0), velocity=1.0)
         a.direction.azimuth = math.pi / 2
         a.direction.elevation = math.pi / 4
-        a.move_next(1.0)
+        a.move(1.0)
         print(a)
-        assert abs(a.location.x - 0.0) < 0.01
-        assert abs(a.location.y - 0.7071) < 0.01
-        assert abs(a.location.h - 0.7071) < 0.01
-        a = Airplane('6 plane1', Location3d(0.0, 0.0, 0.0), Direction3d(0.0, 0.0), Direction3d(), velocity=1.0)
+        assert abs(a.location.x - 0.0) < epsilon
+        assert abs(a.location.y - 0.7071) < epsilon
+        assert abs(a.location.h - 0.7071) < epsilon
+        a = Airplane('6 plane1', Location3d(0.0, 0.0, 0.0), Direction3d(0.0, 0.0), velocity=1.0)
         a.direction.azimuth = math.pi / 2
         a.direction.elevation = math.pi / 2
-        a.move_next(1.0)
+        a.move(1.0)
         print(a)
-        assert abs(a.location.x - 0.0) < 0.01
-        assert abs(a.location.y - 00) < 0.01
-        assert abs(a.location.h - 1) < 0.01
+        assert abs(a.location.x - 0.0) < epsilon
+        assert abs(a.location.y - 00) < epsilon
+        assert abs(a.location.h - 1) < epsilon
 
-    def test_move_to_point():
-        a = Airplane('7 plane1', Location3d(0.0, 0.0, 0.0), Direction3d(0.0, 0.0), Direction3d(), velocity=1.0)
-        x = [a.location.x]
-        y = [a.location.y]
-        z = [a.location.h]
+    def test_calculate_direction():
+        a = Airplane('1 plane1', Location3d(0.0, 0.0, 0.0), Direction3d(0.0, 0.0), velocity=1.0)
+        print(a)
+        location = a.calculate_Direction(Location3d(100, 100, 0))
+        print(location)
+        assert abs(location.azimuth_degree - 45) < epsilon
+        location = a.calculate_Direction(Location3d(-100, -100, 0))
+        print(location)
+        assert abs(location.azimuth_degree - (-135)) < epsilon
+        location = a.calculate_Direction(Location3d(0, 0, 100))
+        print(location)
+        assert abs(location.elevation_degree - 90) < epsilon
+        location = a.calculate_Direction(Location3d(100, 0, 100))
+        print(location)
+        assert abs(location.elevation_degree - 45) < epsilon
+        location = a.calculate_Direction(Location3d(100, 100, math.sqrt(100 ** 2 + 100 ** 2)))
+        print(location)
+        location = a.calculate_Direction(Location3d(100, 100, 100))
+        print(location)
+        assert abs(location.elevation_degree - 35.26) < epsilon
 
-        for _ in range(200):
-            a.move_next_to_point(1.0, Location3d(10, 0, 00))
-            x.append(a.location.x)
-            y.append(a.location.y)
-            z.append(a.location.h)
-            print(a.location)
-        for _ in range(200):
-            a.move_next_to_point(1.0, Location3d(10, 0, 0))
-            x.append(a.location.x)
-            y.append(a.location.y)
-            z.append(a.location.h)
-            print(a.location)
-
-        # Create the figure and plot
-        fig = plt.figure(figsize=(10, 6))
-        ax = plt.axes(projection="3d")
-        ax.plot3D(x, y, z, "blue", linewidth=2)
-
-        # Set labels and title
-        ax.set_xlabel("X")
-        ax.set_ylabel("Y")
-        ax.set_zlabel("Z")
-        ax.set_title("3D Line Connecting Random Points")
-        plt.show()
-
-    test_move_next()
-    test_move_to_point()
+    test_move()
+    test_calculate_direction()
--- PythonExamples/airplane_course/direction3d.py
+++ PythonExamples/airplane_course/direction3d.py
@@ -1,7 +1,17 @@
+import math
+
 class Direction3d:
     def __init__(self, azimuth=0.0, elevation=0.0):
         self.azimuth = azimuth
         self.elevation = elevation
 
+    @property
+    def azimuth_degree(self):
+        return math.degrees(self.azimuth)
+
+    @property
+    def elevation_degree(self):
+        return math.degrees(self.elevation)
+
     def __str__(self):
-        return f'(az={self.azimuth},el={self.elevation})'
+        return f'(az={math.degrees(self.azimuth)},el={math.degrees(self.elevation)})'
