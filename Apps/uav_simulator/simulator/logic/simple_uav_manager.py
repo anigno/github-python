b91db3fd1e9788383ff2db2d748527b901350efe
anigno <anigno@gmail.com>
2023-12-23 22:33:39 +0200

    uav simulator

--- Apps/uav_simulator/simulator/logic/simple_uav_manager.py
+++ Apps/uav_simulator/simulator/logic/simple_uav_manager.py
@@ -10,23 +10,28 @@ class SimpleUavManager:
     IN_LOCATION_DISTANCE = 10
 
     def __init__(self, name: str, uav_params: UavParams, location: Location3d, direction: Direction3d):
+        self.name = name
+        self.uav_params = uav_params
+        self.location = location
+        self.destination = location
+        self.direction = direction
+        self.velocity = 0.0
         self.update_thread = Thread(name='update', target=self.update_thread_start, daemon=True)
-        self.destination = self.uav_actions.location
         self.previous_update_time = 0.0
 
     def start(self):
         self.previous_update_time = time.time()
         self.update_thread.start()
 
-    def fly_to(self, destination: Location3d):
+    def set_destination(self, destination: Location3d):
         self.destination = destination
-        self.uav_actions.direction = self.uav_actions.calculate_Direction_to(destination)
 
     def update_thread_start(self):
         time.sleep(1)
         new_update_time = time.time()
         delta_time = new_update_time - self.previous_update_time
+        direction = self.calculate_Direction_to(self.destination)
         distance = self.uav_actions.calculate_distance_to(self.destination)
         if distance > SimpleUavManager.IN_LOCATION_DISTANCE:
-            self.uav_actions.move(delta_time)
+            self.move(delta_time)
         self.previous_update_time = new_update_time
