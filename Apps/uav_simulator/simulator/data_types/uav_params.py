54b5d1998f2dd967d6f18c79aa6111015ccb50c5
aharon gina <44746052+anigno@users.noreply.github.com>
2023-12-28 16:16:32 +0200

    crc uav

--- Apps/uav_simulator/simulator/data_types/uav_params.py
+++ Apps/uav_simulator/simulator/data_types/uav_params.py
@@ -2,6 +2,10 @@ class UavParams:
     def __init__(self, max_flight_time=60 * 5, max_velocity=10):
         self.max_flight_time = max_flight_time
         self.flight_velocity = max_velocity
+        self.local_ip='127.0.0.1'
+        self.local_port=7100
+        self.buffer_size=60000
+
 
     def __str__(self):
         return f'({self.max_flight_time} {self.flight_velocity})'
--- common/crc_utils.py
+++ common/crc_utils.py
@@ -1,5 +1,5 @@
-import binascii
 import crc8
+import crcmod
 
 from common.bytes_parser import BytesParser
 
@@ -19,22 +19,28 @@ class CrcUtils:
 
     @staticmethod
     def calculate_crc32(data_bytes: bytes) -> bytes:
-        int_crc = binascii.crc32(data_bytes)
-        data_bytes = BytesParser.to_bytes(int_crc, 4)
+        crc32_func = crcmod.predefined.mkCrcFun('crc-32')
+        crc_value = crc32_func(data_bytes)
+        data_bytes = BytesParser.to_bytes(crc_value, 4)
         return data_bytes
 
     @staticmethod
     def verify_crc32(data_bytes: bytes, crc32_val: bytes) -> bool:
-        crc = CrcUtils.calculate_crc32(data_bytes)
-        return crc == crc32_val
+        crc_bytes = CrcUtils.calculate_crc32(data_bytes)
+        return crc_bytes == crc32_val
 
 if __name__ == '__main__':
     from random import randbytes
 
-    bytes1 = randbytes(1024)
-    crc1 = CrcUtils.calculate_crc8(bytes1)
-    b1 = CrcUtils.verify_crc8(bytes1, crc1)
-    print(f'result={b1} crc={crc1} len={len(bytes1)})')
-    bytes2 = bytes1[:-1] + b'1'
-    b2 = CrcUtils.verify_crc8(bytes2, crc1)
-    print(f'result={b2} crc={crc1} len={len(bytes2)})')
+    bytes_array1 = randbytes(1024)
+    bytes_array2 = bytes_array1 + b'1'
+    print(bytes_array1)
+    crc_val1 = CrcUtils.calculate_crc8(bytes_array1)
+    crc_val2 = CrcUtils.calculate_crc8(bytes_array2)
+    assert CrcUtils.verify_crc8(bytes_array1, crc_val1)
+    assert not CrcUtils.verify_crc8(bytes_array1, crc_val2)
+
+    crc_val1 = CrcUtils.calculate_crc32(bytes_array1)
+    crc_val2 = CrcUtils.calculate_crc32(bytes_array2)
+    assert CrcUtils.verify_crc32(bytes_array1, crc_val1)
+    assert not CrcUtils.verify_crc32(bytes_array1, crc_val2)
