ca3e4b5ce65402a642ed6d05063e75e52ce5de27
anigno <anigno@gmail.com>
2024-01-09 21:31:12 +0200

    grpc

--- PythonExamples/Communication/grpc/client.py
+++ PythonExamples/Communication/grpc/client.py
@@ -1,21 +1,29 @@
 import time
+from threading import Thread
 
 import grpc
 import messages_pb2
 import messages_pb2_grpc
 
-def run():
-    channel = grpc.insecure_channel('localhost:50051')
-    stub = messages_pb2_grpc.TextMessageServiceStub(channel)
+class GrpcClient:
+    def __init__(self):
+        channel = grpc.insecure_channel('localhost:50051')
+        self.stub = messages_pb2_grpc.TextMessageServiceStub(channel)
+        self.client_thread = Thread(target=self.client_thread_start(), daemon=True)
 
-    while True:
-        try:
-            message = messages_pb2.TextMessage(text=f"hello from client: {time.time()}")
-            response = stub.ClientSendMessage(message)
-            print("Client received response:", response.text)
-        except Exception as ex:
-            print(ex)
-        time.sleep(3)
+    def client_thread_start(self):
+        while True:
+            try:
+                message = messages_pb2.TextMessage(text=f"hello from client: {time.time()}")
+                response = self.stub.ClientSendMessage(message)
+                print("Client received response:", response.text)
+            except Exception as ex:
+                print(ex)
+            time.sleep(3)
+
+    def start(self):
+        self.client_thread.start()
 
 if __name__ == '__main__':
-    run()
+    grpc_client = GrpcClient()
+    grpc_client.start()
--- PythonExamples/Communication/grpc/server.py
+++ PythonExamples/Communication/grpc/server.py
@@ -1,23 +1,32 @@
+from threading import Thread
+
 import grpc
 from concurrent import futures
 import time
 import messages_pb2
 import messages_pb2_grpc
 
-class TextMessageService(messages_pb2_grpc.TextMessageServiceServicer):
+class TextMessageReceivingService(messages_pb2_grpc.TextMessageServiceServicer):
     def ClientSendMessage(self, request, context):
         print(f'server received: {request.text}')
         response = messages_pb2.ResponseMessage()
         response.text = "Server ack for: " + request.text
         return response
 
-def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    messages_pb2_grpc.add_TextMessageServiceServicer_to_server(TextMessageService(), server)
-    server.add_insecure_port('[::]:50051')
-    server.start()
-    print("Server started. Listening on port 50051.")
-    server.wait_for_termination()
+class GrpcServer:
+    def __init__(self):
+        self.server_thread = Thread(target=self.server_thread_start(), daemon=True)
+
+    def server_thread_start(self):
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        messages_pb2_grpc.add_TextMessageServiceServicer_to_server(TextMessageReceivingService(), server)
+        server.add_insecure_port('[::]:50051')
+        server.start()
+        server.wait_for_termination()
+
+    def start(self):
+        self.server_thread.start()
 
 if __name__ == '__main__':
-    serve()
+    grpc_server = GrpcServer()
+    grpc_server.start()
