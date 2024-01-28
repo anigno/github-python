f8e252b56a8456dd5292e5816cefb0967222a6f5
anigno <anigno@gmail.com>
2024-01-28 14:28:08 +0200

    face swap by InsightFace Swapper working tested

--- Apps/face_swap/face_swap.py
+++ Apps/face_swap/face_swap.py
@@ -45,27 +45,27 @@ class FaceSwap:
         # cv2.imwrite("./t1_swapped2.jpg", res_image)
 
 if __name__ == '__main__':
-    FaceSwap.do_swap(source_img='1.jpg', source_face_index=0, target_img='2.jpg', result_img='res.jpg')
+    FaceSwap.do_swap(source_img=r'D:\Downloads\src1.jpg', source_face_index=0, target_img=r'D:\Downloads\tar1.png', result_img='res.jpg')
 
-    def sample():
-        app = FaceAnalysis(name='buffalo_l')
-        app.prepare(ctx_id=0, det_size=(640, 640))
-        swapper = insightface.model_zoo.get_model('inswapper_128.onnx', download=True, download_zip=True)
-        shutil.copy('t1.jpg', r'C:\anaconda3\Lib\site-packages\insightface\data\images')
-        img = ins_get_image('t1')
-        faces = app.get(img)
-        faces = sorted(faces, key=lambda x: x.bbox[0])
-        assert len(faces) == 6
-        source_face = faces[2]
-        res = img.copy()
-        for face in faces:
-            res = swapper.get(res, face, source_face, paste_back=True)
-        cv2.imwrite("./t1_swapped.jpg", res)
-        res = []
-        for face in faces:
-            _img, _ = swapper.get(img, face, source_face, paste_back=False)
-            res.append(_img)
-        res = np.concatenate(res, axis=1)
-        cv2.imwrite("./t1_swapped2.jpg", res)
+    # def sample():
+    #     app = FaceAnalysis(name='buffalo_l')
+    #     app.prepare(ctx_id=0, det_size=(640, 640))
+    #     swapper = insightface.model_zoo.get_model('inswapper_128.onnx', download=True, download_zip=True)
+    #     shutil.copy('t1.jpg', r'C:\anaconda3\Lib\site-packages\insightface\data\images')
+    #     img = ins_get_image('t1')
+    #     faces = app.get(img)
+    #     faces = sorted(faces, key=lambda x: x.bbox[0])
+    #     assert len(faces) == 6
+    #     source_face = faces[2]
+    #     res = img.copy()
+    #     for face in faces:
+    #         res = swapper.get(res, face, source_face, paste_back=True)
+    #     cv2.imwrite("./t1_swapped.jpg", res)
+    #     res = []
+    #     for face in faces:
+    #         _img, _ = swapper.get(img, face, source_face, paste_back=False)
+    #         res.append(_img)
+    #     res = np.concatenate(res, axis=1)
+    #     cv2.imwrite("./t1_swapped2.jpg", res)
 
     # sample()
