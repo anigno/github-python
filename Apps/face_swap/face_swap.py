b6a0624f13231a16b691c35b756ff48652fdce71
anigno <anigno@gmail.com>
2024-01-31 20:08:27 +0200

    face swap changes

--- Apps/face_swap/face_swap.py
+++ Apps/face_swap/face_swap.py
@@ -27,6 +27,8 @@ class FaceSwap:
         src_img = ins_get_image(source_img_name)
         src_faces = app.get(src_img)
         src_faces = sorted(src_faces, key=lambda x: x.bbox[0])
+        if not src_faces:
+            return False
         source_face = src_faces[source_face_index]
         tar_img = ins_get_image(target_img_name)
         tar_faces = app.get(tar_img)
@@ -35,8 +37,9 @@ class FaceSwap:
         for face in tar_faces:
             res_image = swapper.get(res_image, face, source_face, paste_back=True)
         cv2.imwrite(f"./{result_img}", res_image)
-        os.remove(os.path.join(anaconda_path, source_img))
-        os.remove(os.path.join(anaconda_path, target_img))
+        return True
+        # os.remove(os.path.join(anaconda_path, source_img))
+        # os.remove(os.path.join(anaconda_path, target_img))
         # res_image = []
         # for face in src_faces:
         #     _img, _ = swapper.get(src_img, face, source_face, paste_back=False)
@@ -44,10 +47,20 @@ class FaceSwap:
         # res_image = np.concatenate(res_image, axis=1)
         # cv2.imwrite("./t1_swapped2.jpg", res_image)
 
-if __name__ == '__main__':
-    FaceSwap.do_swap(source_img=r'D:\Downloads\src1.jpg', source_face_index=0, target_img=r'D:\Downloads\tar1.png', result_img='res.jpg')
+    @staticmethod
+    def swap_multi(main_dir):
+        src_dir = os.path.join(main_dir, 'src')
+        tar_dir = os.path.join(main_dir, 'tar')
+        res_dir = os.path.join(main_dir, 'res')
+        for j, tar in enumerate(os.listdir(tar_dir)):
+            for i, src in enumerate(os.listdir(src_dir)):
+                result=FaceSwap.do_swap(os.path.join(src_dir, src), 0, os.path.join(tar_dir, tar), 'res.jpg')
+                if result:
+                    shutil.move('res.jpg', os.path.join(res_dir, f'res_{j}_{i}.jpg'))
 
-    # def sample():
+if __name__ == '__main__':
+    FaceSwap.swap_multi(r'D:\shared\swap\keren_ch')
+    # def sample(),:
     #     app = FaceAnalysis(name='buffalo_l')
     #     app.prepare(ctx_id=0, det_size=(640, 640))
     #     swapper = insightface.model_zoo.get_model('inswapper_128.onnx', download=True, download_zip=True)
